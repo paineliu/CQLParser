18dac121898160ac2b1be53ec7239aa2a01f20cd
paineliu <liutingchao@hotmail.com>
2024-03-08 22:02:47 +0800

    add ignore x64 Win32

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-﻿/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
- * Use of this file is governed by the BSD 3-clause license that
- * can be found in the LICENSE.txt file in the project root.
- */
-
- //
- //  main.cpp
- //  antlr4-cpp-demo
- //
- //  Created by Mike Lischke on 13.03.16.
- //
-
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <stdio.h>
 #include "antlr4-runtime.h"
@@ -18,7 +6,6 @@
 #include "./generated/CQLParser.h"
 #include "./generated/CQLVisitor.h"
 #include "./generated/CQLBaseVisitor.h"
-#include <Windows.h>
 
 #pragma execution_character_set("utf-8")
 using namespace antlr4;
